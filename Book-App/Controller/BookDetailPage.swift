7d2be88b24fd72bda2e508086b0ff0af9e0ad9e7
Akincetin19 <119444077+Akincetin19@users.noreply.github.com>
2023-02-21 16:48:30 +0300

    Finished BookDetail Page

--- Book-App/Controller/BookDetailPage.swift
+++ Book-App/Controller/BookDetailPage.swift
@@ -20,48 +20,122 @@ class BookDetailPage: UIViewController {
         return label
     }()
     
+    lazy var bookAuthorLabel: UILabel = {
+       
+        let label = UILabel(frame: .zero)
+        label.text = selectedBook?.Author
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: 20)
+        return label
+        
+    }()
+    lazy var bookPriceLabel: UILabel = {
+       
+        let label = UILabel(frame: .zero)
+        label.text = "Fiyat - \(selectedBook?.Price ?? "")"
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: 40)
+        return label
+        
+    }()
+    
+    fileprivate var addBasketButton = {
+        
+        let button = UIButton(frame: .zero)
+        button.setTitle("Sepete Ekle", for: .normal)
+        button.backgroundColor = .red
+        button.setTitleColor(UIColor.white, for: .normal)
+        return button
+    }()
+    
     fileprivate let bookImage = UIImageView()
     private let scrollView = UIScrollView(frame: .zero)
     private let contentView = UIView()
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        configureView()
+        setupScrollview()
+        setupContentView()
+        configureNavigationItems()
+        configureBookImage()
+        configureDescriptionLabel()
+        configureBookAuthorLabel()
+        configureBookPriceLabel()
+        configureAddBasketButton()
+    }
+    
+    fileprivate func configureView() {
         view.backgroundColor = .white
-        
-        
         view.addSubview(scrollView)
+    }
+    fileprivate func configureAddBasketButton() {
+        
+        addBasketButton.layer.cornerRadius = 8
+        addBasketButton.addTarget(self, action: #selector(addBasketClicked), for: .touchUpInside)
+        addBasketButton.anchor(top: bookPriceLabel.bottomAnchor, leading: contentView.leadingAnchor, bottom: nil, trailing: contentView.trailingAnchor, padding: .init(top: 16, left: 16, bottom: 0, right: 16), size: .init(width: 0, height: 50))
+        
+    }
+    @objc fileprivate func addBasketClicked() {
+        print("Add Basket Clicked")
+    }
+    
+    fileprivate func configureBookAuthorLabel() {
+     
+        bookAuthorLabel.text = "Yazar - \(bookAuthorLabel.text!) TL"
+        bookAuthorLabel.textColor = .blue
+        bookAuthorLabel.anchor(top: bookImage.bottomAnchor, leading: contentView.leadingAnchor, bottom: nil, trailing: contentView.trailingAnchor, padding: .init(top: 16, left: 8, bottom: 0, right: 8))
+        let gesture = UITapGestureRecognizer(target: self, action: #selector(authorClicked))
+        bookAuthorLabel.isUserInteractionEnabled = true
+        bookAuthorLabel.addGestureRecognizer(gesture)
+    }
+    @objc func authorClicked(sender:UITapGestureRecognizer) {
+        print("author clicked")
+        
+        // yazarın kitapları getirilecek
+        navigationController?.pushViewController(UIViewController(), animated: true)
+    }
+    fileprivate func configureDescriptionLabel() {
+        descriptionLabel.numberOfLines = 0
+        descriptionLabel.anchor(top: addBasketButton.bottomAnchor, leading: contentView.leadingAnchor, bottom: contentView.bottomAnchor, trailing: contentView.trailingAnchor, padding: .init(top: 16, left: 16, bottom: 16, right: 16))
+    }
+    fileprivate func configureBookPriceLabel() {
+        
+        bookPriceLabel.anchor(top: bookAuthorLabel.bottomAnchor, leading: contentView.leadingAnchor, bottom: nil, trailing: contentView.trailingAnchor, padding: .init(top: 16, left: 16, bottom: 16, right: 16))
+    }
+    
+    fileprivate func configureBookImage() {
+        
+        let url = URL(string: selectedBook?.Url ?? "")
+        guard let url = url else {return}
+        bookImage.sd_setImage(with: url)
+        bookImage.contentMode = .scaleAspectFit
+        bookImage.translatesAutoresizingMaskIntoConstraints = false
+        bookImage.anchor(top: contentView.topAnchor, leading: contentView.leadingAnchor, bottom: nil, trailing: contentView.trailingAnchor, padding: .init(top: 16, left: 0, bottom: 0, right: 0))
+        
+    }
+    fileprivate func setupScrollview() {
         scrollView.translatesAutoresizingMaskIntoConstraints = false
-        contentView.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(scrollView)
         scrollView.addSubview(contentView)
-                
         scrollView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         scrollView.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
         scrollView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
         scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
     
+    }
+    fileprivate func setupContentView() {
+        contentView.translatesAutoresizingMaskIntoConstraints = false
         contentView.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor).isActive = true
         contentView.widthAnchor.constraint(equalTo: scrollView.widthAnchor).isActive = true
         contentView.topAnchor.constraint(equalTo: scrollView.topAnchor).isActive = true
         contentView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor).isActive = true
-        
-        configureNavigationItems()
-        let url = URL(string: selectedBook?.Url ?? "")
-        guard let url = url else {return}
-        bookImage.sd_setImage(with: url)
-        bookImage.contentMode = .scaleAspectFit
-        scrollView.addSubview(contentView)
         contentView.addSubview(bookImage)
         contentView.addSubview(descriptionLabel)
-    
-        bookImage.translatesAutoresizingMaskIntoConstraints = false
-
-        bookImage.anchor(top: contentView.topAnchor, leading: contentView.leadingAnchor, bottom: nil, trailing: contentView.trailingAnchor, padding: .init(top: 16, left: 0, bottom: 0, right: 0))
-        
-        
-        descriptionLabel.numberOfLines = 0
-        descriptionLabel.anchor(top: bookImage.bottomAnchor, leading: contentView.leadingAnchor, bottom: contentView.bottomAnchor, trailing: contentView.trailingAnchor, padding: .init(top: 16, left: 16, bottom: 16, right: 16))
+        contentView.addSubview(bookAuthorLabel)
+        contentView.addSubview(bookPriceLabel)
+        contentView.addSubview(addBasketButton)
     }
 
     fileprivate func configureNavigationItems() {
@@ -71,10 +145,8 @@ class BookDetailPage: UIViewController {
             view.backgroundColor = .black
             return view
         }()
-        
         view.addSubview(border)
         border.anchor(top: view.safeAreaLayoutGuide.topAnchor, leading: view.leadingAnchor, bottom: nil, trailing: view.trailingAnchor, size: .init(width: 0, height: 1))
-        
         title = selectedBook?.BookName
         let heart = UIBarButtonItem(image: UIImage(systemName: "heart.fill"), style: .plain, target: self, action: nil)
         let search = UIBarButtonItem(barButtonSystemItem: .search, target: self, action: nil)
